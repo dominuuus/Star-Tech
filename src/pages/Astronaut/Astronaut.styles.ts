29c13241e2a0131a676f8330dfec0f95007fa599
Domingos <dmgssantiago@gmail.com>
2025-04-25 14:20:47 -0300

    Conclusão do MissionCard

--- src/pages/Astronaut/Astronaut.styles.ts
+++ src/pages/Astronaut/Astronaut.styles.ts
@@ -1,75 +1,79 @@
 import styled from "styled-components";
 
 export const AstronautContainer = styled.main`
-  display: flex;
-`;
+    display: flex;
+`
 
 export const AstronautSectionTwo = styled.section`
-  display: flex;
-  flex-direction: column;
-`;
+    display: flex;
+    flex-direction: column;
+`
 
 export const AstronautSectionOne = styled.section`
-  display: flex;
-  flex-direction: column;
-`;
+    display: flex;
+    flex-direction: column;
+`
 
 export const SectionTwoContentOne = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+    display: flex;
+    flex-direction: column;
+`
 
 export const MascotAchieveNewsContainer = styled.div`
-  display: grid;
-  grid-template-columns: 40% 30% 30%;
-  gap: 0.5rem;
-`;
+    display: grid;
+    grid-template-columns: 40% 30% 30%;
+    gap: 0.5rem;
+`
 
-export const NewsContent = styled.div``;
+export const NewsContent = styled.div`
+
+`
 
 export const SectionTwoContentTwo = styled.div`
-  margin-top: 1rem;
-  display: grid;
-  grid-template-columns: 50% 50%;
-  gap: 0.5rem;
-`;
+    margin-top: 1rem;
+    display: grid;
+    grid-template-columns: 50% 50%;
+    gap: 0.5rem;
+
+`
 
 export const MissionStatusContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: gray;
-  width: 28rem;
-  height: 20rem;
-  border-radius: 0.75rem;
-  padding: 1rem;
-`;
+    display: flex;
+    flex-direction: column;
+    background-color: gray;
+    width: 28rem;
+    height: 20rem;
+    border-radius: 0.75rem;
+    padding: 1rem;
+    gap: 15px;
+`
 export const CommandCenterContent = styled.div`
-  background-color: gray;
-  width: 28rem;
-  height: 20rem;
-  border-radius: 0.75rem;
-`;
+    background-color: gray;
+    width: 28rem;
+    height: 20rem;
+    border-radius: 0.75rem;
+`
 
 export const SpaceshipContent = styled.div`
-  position: absolute;
-  margin-top: 10.5rem;
-  width: 40rem;
-  height: 9.5rem;
-  background-color: gray;
-  border-radius: 0.75rem;
-  padding: 1rem;
-`;
+    position: absolute;
+    margin-top: 10.5rem;
+    width: 40rem;
+    height: 9.5rem;
+    background-color: gray;
+    border-radius: 0.75rem;
+    padding: 1rem;
+`
 
 export const MascotContent = styled.div`
-  height: 10rem;
-  background-color: gray;
-  border-radius: 0.75rem;
-  padding: 1rem;
-`;
+    height: 10rem;
+    background-color: gray;
+    border-radius: 0.75rem;
+    padding: 1rem;
+`
 
 export const AchievementContent = styled.div`
-  height: 10rem;
-  background-color: gray;
-  border-radius: 0.75rem;
-  padding: 1rem;
-`;
+    height: 10rem;
+    background-color: gray;
+    border-radius: 0.75rem;
+    padding: 1rem;
+`
