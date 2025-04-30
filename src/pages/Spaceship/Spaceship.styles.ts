a33beeff1a5e53d4f1364477b7acc9f3e27b75c3
Domingos <dmgssantiago@gmail.com>
2025-04-30 12:39:36 -0300

    Ajustes da página da nave

--- src/pages/Spaceship/Spaceship.styles.ts
+++ src/pages/Spaceship/Spaceship.styles.ts
@@ -1,21 +1,37 @@
 import styled from "styled-components";
 
 export const SpaceshipContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
+  display: flex;
+  flex-direction: row;
 
-    h1 {
-        font-size: 2rem;
-    }
-
-    img {
-        height: 30rem;
-        position: absolute;
-    }
-
-`
+  h1 {
+    font-size: 2rem;
+  }
+`;
 
 export const MissionUpdatesContent = styled.div`
-    width: 20rem;
-    margin-left: 50rem;
-`
+  width: 20rem;
+  margin-left: 35rem;
+`;
+
+export const ExploredPlanetContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  img {
+    height: 5rem;
+    cursor: pointer;
+
+    &:hover {
+        transform: scale(1.2);
+        }
+  }
+`;
+
+export const SpaceshipCardContainer = styled.div`
+  img {
+    height: 30rem;
+    position: absolute;
+
+  }
+`;
--- src/pages/Spaceship/Spaceship.tsx
+++ src/pages/Spaceship/Spaceship.tsx
@@ -1,14 +1,26 @@
 import images from "../../assets/images";
+import planets from "../../assets/planets";
 import { NewsMission } from "../../components/common/NewsMission/NewsMission";
-import { MissionUpdatesContent, SpaceshipContainer } from "./Spaceship.styles";
+import { ExploredPlanetContainer, MissionUpdatesContent, SpaceshipCardContainer, SpaceshipContainer } from "./Spaceship.styles";
 
 export function Spaceship() {
     return (
         <>
         <SpaceshipContainer>
         <div>
+            <div>
             <h1>Codestar-1</h1>
-            <img src={images.nave} alt="" />
+            <ExploredPlanetContainer>
+                <img src={planets.coreon} alt="" />
+                <img src={planets.pyron} alt="" />
+                <img src={planets.aeris} alt="" />
+                <img src={planets.crystalia} alt="" />
+                <img src={planets.verdax} alt="" />
+            </ExploredPlanetContainer>
+            </div>
+            <SpaceshipCardContainer>
+                <img src={images.nave} alt="" />
+            </SpaceshipCardContainer>
         </div>
 
         <MissionUpdatesContent>
