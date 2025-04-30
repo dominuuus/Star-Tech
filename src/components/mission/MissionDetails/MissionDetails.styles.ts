29e32a96814e8dfdeed4c89af37dbbcf62b845f4
Malveira16 <joa.malveira@gmail.com>
2025-04-30 14:22:20 -0300

    Adição da barra de prgresso na página de missões

--- src/components/mission/MissionDetails/MissionDetails.styles.ts
+++ src/components/mission/MissionDetails/MissionDetails.styles.ts
@@ -28,7 +28,41 @@ export const MissionDetailsBody = styled.div `
         padding-top: 25px;
         font-weight: bold;
     }
+    h4.Progresso {
+        padding-top: 90px;
+    }
     li {
         padding-left: 30px;
     }
+    p {
+        font-size: 10px;
+        padding-bottom: 10px;
+    }
+   
+`
+export const ProgressContainer = styled.div `
+
+`
+export const ProgressMissionBar = styled.div `
+    background-color: #efefef;
+    border-radius: 30px;
+    width: 40%;
+    height: 60px; 
+    position: relative;
+    overflow: hidden;
+  
+`
+interface ProgressProps {
+    width: string;
+  }
+export const ProgressMissionBarContent = styled.div<ProgressProps> `
+    background-color: red;
+    width: ${(props) => props.width}; 
+    height: 100%;
+    border-radius: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: red;
+    font-size: 10px;
 `
--- src/components/mission/MissionDetails/MissionDetails.tsx
+++ src/components/mission/MissionDetails/MissionDetails.tsx
@@ -4,6 +4,9 @@ import {
   MissionDetailsContainer,
   MissionDetailsHead,
   MissionTitle,
+  ProgressContainer,
+  ProgressMissionBar,
+  ProgressMissionBarContent,
 } from "./MissionDetails.styles";
 
 export function MissionDetails() {
@@ -22,13 +25,32 @@ export function MissionDetails() {
         </MissionDetailsHead>
         <hr />
         <MissionDetailsBody>
-          <span>Detectamos trechos duplicados em módulos distintos. Isso consome energia da nave e pode gerar inconsistência em caso de mudanças futuras. </span>
+          <span>
+            Detectamos trechos duplicados em módulos distintos. Isso consome
+            energia da nave e pode gerar inconsistência em caso de mudanças
+            futuras.{" "}
+          </span>
           <h4>Objetivo Técnico:</h4>
           <ul>
-            <li>Identificar códigos duplicados que podem ser extraidos para funções reutilizáveis. </li>
+            <li>
+              Identificar códigos duplicados que podem ser extraidos para
+              funções reutilizáveis.{" "}
+            </li>
             <li>refatorar para reaproveitamento de lógica comum.</li>
             <li>Testar para que o comportamento se mantenha intacto</li>
           </ul>
+          <ProgressContainer>
+          <h4 className="Progresso">Progresso</h4>
+          <p>O prazo da missão vence em dois dias</p>
+          <ProgressMissionBar>
+            <ProgressMissionBarContent width="40%">
+              40%
+            </ProgressMissionBarContent>
+          </ProgressMissionBar>
+          <div>
+            <h4>40%</h4>
+          </div>
+          </ProgressContainer>
         </MissionDetailsBody>
       </MissionDetailsContainer>
     </>
