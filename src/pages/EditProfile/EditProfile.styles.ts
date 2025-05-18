54a5bd322fec5d5ed33d1cf64af1f6af80f08ab1
Malveira16 <joa.malveira@gmail.com>
2025-05-18 18:57:15 -0300

    Mudanças na tela de perfil

--- src/pages/EditProfile/EditProfile.styles.ts
+++ src/pages/EditProfile/EditProfile.styles.ts
@@ -1,8 +1,68 @@
 import { styled } from "styled-components";
+import { UserProfile } from "../../components/common/UserProfile/UserProfile";
 
 export const Profile = styled.div`
   display: flex;
   flex-direction: row;
+  gap: 300px;
+`;
+
+export const PlanetAndCredentials = styled.div `
+  display: flex;
+  flex-direction: column;
+  width: 25%;
+`
+
+export const AvatarCircle = styled.div `
+  width: 940px;
+  height: 940px;
+  border: 2px solid;
+  border-radius: 50%;
+  overflow: visible;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+export const OrbitRing = styled.div`
+  position: absolute;
+  inset: 0;                      /* ocupa todo o AvatarCircle */
+  animation: spin 20s linear infinite;
+
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  /* planetas genéricos */
+  .planet {
+    position: absolute;
+    width: 18px;
+    height: 18px;
+    border-radius: 50%;
+    background: ${({ theme }) => theme.colors.primary};
+  }
+
+  /* planeta 1 – topo */
+  .p1 {
+    top: -9px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  /* planeta 2 – direita */
+  .p2 {
+    bottom: -9px;
+    left: calc(50% - 9px);
+    background: black;
+  }
 `;
 
 export const AvatarContainer = styled.div`
@@ -11,14 +71,21 @@ export const AvatarContainer = styled.div`
   display: flex;
   flex-direction: row;
   gap: 0.1px;
+   
+  
 `;
 export const TightAvatarContainer = styled(AvatarContainer)`
   & > :first-child {
     margin-right: 0 !important;
     padding-right: 0 !important;
   }
+ 
 `;
 
+export const SmallUserProfile = styled(UserProfile)`
+    transform: scale(0.75);
+  transform-origin: center;
+`
 export const EditAvatarContent = styled.div`
   display: flex;
   flex-direction: column;
@@ -42,3 +109,9 @@ export const EditAvatar = styled.div`
     color: white;
   }
 `;
+
+export const MascotsAndAchievements = styled.div `
+  display: flex;
+  flex-direction: column;
+  width: 25%;
+`
--- src/pages/EditProfile/EditProfile.tsx
+++ src/pages/EditProfile/EditProfile.tsx
@@ -1,8 +1,12 @@
-import { UserProfile } from "../../components/common/UserProfile/UserProfile";
 import {
+  AvatarCircle,
   EditAvatar,
   EditAvatarContent,
+  MascotsAndAchievements,
+  OrbitRing,
+  PlanetAndCredentials,
   Profile,
+  SmallUserProfile,
   TightAvatarContainer,
 } from "./EditProfile.styles";
 import { PencilSimpleLine, UserCircleGear } from "phosphor-react";
@@ -11,21 +15,34 @@ export function EditProfile() {
   return (
     <>
       <Profile>
+        <PlanetAndCredentials>
+          <span>Planeta atual</span>
+          <span>Suas Credenciais</span>
+        </PlanetAndCredentials>
         <TightAvatarContainer>
-          <UserProfile></UserProfile>
+          <AvatarCircle>
+            <SmallUserProfile />
+            <OrbitRing>
+              <span className="planet p1" />
+              <span className="planet p2" />
+            </OrbitRing>
+          </AvatarCircle>
+
           <EditAvatarContent>
             <EditAvatar>
               <PencilSimpleLine size={22} />
               <span>Editar</span>
             </EditAvatar>
             <EditAvatar>
-                <UserCircleGear size={32} />
-                <span>Personalizar</span>
+              <UserCircleGear size={32} />
+              <span>Personalizar</span>
             </EditAvatar>
           </EditAvatarContent>
         </TightAvatarContainer>
-        <div></div>
-        <div></div>
+        <MascotsAndAchievements>
+          <span>Mascotes</span>
+          <span>Conquistas</span>
+        </MascotsAndAchievements>
       </Profile>
     </>
   );
